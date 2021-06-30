88173db1b463663acbc60eb6decd9b66e07f24b5
sab221b <sab221b@gmail.com>
2021-07-01 03:42:20 +0530

    [changes]: package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ['DIN', 'sans-serif'],
+      body: ['DIN', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        primary: '#07c806',
+        background: {
+          100: '#F2F2F2',
+          200: '999999',
+        },
+        fontColor: {
+          100: '#ADB7BC',
+        },
+        socialColor: {
+          facebook: '#3B5798',
+        }
+      }
+
+    },
   },
   variants: {
     extend: {},
